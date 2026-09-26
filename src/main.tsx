a350495282407cad03ecc8bdf8faa9fe1abdd6a5
koseny <robert.kosina@gmail.com>
2026-09-26 21:32:39 +0200

    EB-2: route entry point through application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { firebaseIdentityAdapter } from "./adapters/identity/firebaseIdentity";
-import { AuthShell } from "./features/auth/AuthShell";
+import { App } from "./app/App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthShell identity={firebaseIdentityAdapter} />
+    <App />
   </React.StrictMode>,
 );
